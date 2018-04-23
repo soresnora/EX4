c41daf8167dda8144cfd58b0bbae16d651d8b637
soresnora <36822062+soresnora@users.noreply.github.com>
2018-04-23 17:10:30 +0200

    Add files via upload

---
+++ exe4_kesz.py
@@ -0,0 +1,31 @@
+def exe4(string):
+    kerdes=input("Milyen nyelvre fordít? eng/ pig")
+    if kerdes=="pig":
+        print(engtopig(string))
+    else:
+        print(pigtoeng(string))
+
+
+def engtopig(string):
+    tordel = string.split()
+    mondat=''
+    for i in tordel:
+        szavak=i[1:]+i[0] + "ay"
+        mondat+=szavak
+        mondat+=' '
+    valasz=str.capitalize(mondat)
+    return valasz
+
+
+def pigtoeng(string):
+    tordel=string.split()
+    mondat = ''
+    for i in tordel:
+        szavak=i[-3]+i[0:-3]
+        mondat+=szavak
+        mondat+=' '
+    valasz=str.capitalize(mondat)
+    return valasz
+
+mit=input("Szöveg: ")
+exe4(mit)
